bd21a6175c4d356c5675721faea585657e08b0b1
Smd.Afnan Ahmed <smd.afnan786@gmail.com>
2025-07-06 11:13:20 +0000

    ok guide me again thru this, the exact steps to do this in the firebase

--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,27 @@
+// This file must be in the public folder.
 
-// Use the Firebase V9 JS SDK for the service worker
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+// Scripts for Firebase
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
-// This is a special variable that will be replaced by the query parameter.
-const firebaseConfigStr = new URL(location).searchParams.get("firebaseConfig");
+// Get Firebase config from URL query parameter
+const urlParams = new URLSearchParams(self.location.search);
+const firebaseConfig = JSON.parse(urlParams.get('firebaseConfig'));
 
-if (firebaseConfigStr) {
-    const firebaseConfig = JSON.parse(decodeURIComponent(firebaseConfigStr));
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-    firebase.initializeApp(firebaseConfig);
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
 
-    const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+  console.log('[sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/logo.png', // A default icon for notifications
+  };
 
-    messaging.onBackgroundMessage((payload) => {
-        console.log(
-            "[sw.js] Received background message ",
-            payload
-        );
-
-        const notificationTitle = payload.notification.title;
-        const notificationOptions = {
-            body: payload.notification.body,
-            icon: '/logo.png' // Make sure you have a logo.png in your public folder
-        };
-
-        self.registration.showNotification(notificationTitle, notificationOptions);
-    });
-} else {
-    console.log("Firebase config not found in service worker. Notifications will not work in background.");
-}
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
